469ceb0fa64d323dd431134d17f616e721837569
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2020-03-24 20:03:36 +0100

    developed the main stack

--- lib/memeages-back-stack.ts
+++ lib/memeages-back-stack.ts
@@ -1,9 +1,52 @@
 import * as cdk from '@aws-cdk/core';
+import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
+import { Function as LambdaFunction, Runtime as LambdaRuntime, Code as LambdaSourceCode, Runtime } from '@aws-cdk/aws-lambda';
+import { Bucket } from '@aws-cdk/aws-s3';
+
+
+class Utilities {
+
+  /** JSON containing the name of every resources in the project */
+  public static RESOURCE_NAMES = require("../conf/resource-names.json")
+  /** Path of the folder containing the Lambdas source code files */
+  public static LAMBDA_SOURCE_CODE_FOLDER = __dirname + "\\lambdas\\"
+  /** Default Lambda handler function name for every Lambda functions */
+   public static LAMBDA_HANDLER_NAME = "lambda_handler"
+  
+  /** Returns the Lambda handler name (string) for a given Lambda source code filename WITHOUT FILE EXTENSION (string) */
+  public static getLambdaHandlerByLambdaSourceCodeFilename(lambdaSourceCodeFilename: string) {
+    return lambdaSourceCodeFilename + "." + Utilities.LAMBDA_HANDLER_NAME
+  }
+}
+
 
 export class MemeagesBackStack extends cdk.Stack {
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create a S3 bucket containing all the memes (image files)
+    const bucket = new Bucket(this, Utilities.RESOURCE_NAMES.s3.memesBucket)
+
+    // create a Lambda function handling basic CRUD operations on the memes in the S3 folder
+    const crudLambda = new LambdaFunction(this, Utilities.RESOURCE_NAMES.lambda.memesCrudLambda.lambdaName, {
+      runtime: Runtime.PYTHON_3_8,
+      handler: Utilities.getLambdaHandlerByLambdaSourceCodeFilename(Utilities.RESOURCE_NAMES.lambda.memesCrudLambda.lambdaSourceCode),
+      code: LambdaSourceCode.fromAsset(Utilities.LAMBDA_SOURCE_CODE_FOLDER)
+    })
+
+    // create a DynamoDB table storing information regarding the uploaded memes in the S3 bucket
+    const informationTable = new Table(this, Utilities.RESOURCE_NAMES.dynamoDb.memesInformationTable, {
+      partitionKey: {
+        name: 'uri',
+        type: AttributeType.STRING
+      }
+    })
+
+    // create a REST API backed by the previously created CRUD Lambda function
+    const api = new LambdaRestApi(this, Utilities.RESOURCE_NAMES.apiGw.memesApi, {
+      handler: crudLambda
+    })
   }
 }
