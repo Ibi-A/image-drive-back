67af86af9f174775d641990e7bbac41c466ef775
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2020-03-24 23:32:05 +0100

    updated stack to create a Lambda layer with boto3 + imported boto3

--- lib/memeages-back-stack.ts
+++ lib/memeages-back-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
 import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
-import { Function as LambdaFunction, Runtime as LambdaRuntime, Code as LambdaSourceCode, Runtime } from '@aws-cdk/aws-lambda';
+import { Function as LambdaFunction, Runtime as LambdaRuntime, Code as LambdaSourceCode, LayerVersion } from '@aws-cdk/aws-lambda';
 import { Bucket } from '@aws-cdk/aws-s3';
 
 
@@ -11,8 +11,10 @@ class Utilities {
   public static RESOURCE_NAMES = require("../conf/resource-names.json")
   /** Path of the folder containing the Lambdas source code files */
   public static LAMBDA_SOURCE_CODE_FOLDER = __dirname + "\\lambdas\\"
+  /** Path of the folder containing the Lambdas layers files */
+  public static LAMBDA_LAYERS_FOLDER = __dirname + "\\lambdas\\" + "\\layers\\"
   /** Default Lambda handler function name for every Lambda functions */
-   public static LAMBDA_HANDLER_NAME = "lambda_handler"
+  public static LAMBDA_HANDLER_NAME = "lambda_handler"
   
   /** Returns the Lambda handler name (string) for a given Lambda source code filename WITHOUT FILE EXTENSION (string) */
   public static getLambdaHandlerByLambdaSourceCodeFilename(lambdaSourceCodeFilename: string) {
@@ -29,11 +31,19 @@ export class MemeagesBackStack extends cdk.Stack {
     // create a S3 bucket containing all the memes (image files)
     const bucket = new Bucket(this, Utilities.RESOURCE_NAMES.s3.memesBucket)
 
+    // create a Lambda layer containing boto3
+    const crudLambdaLayer = new LayerVersion(this, Utilities.RESOURCE_NAMES.layer.memesCrudLambda, {
+      code: LambdaSourceCode.fromAsset(Utilities.LAMBDA_LAYERS_FOLDER),
+      compatibleRuntimes: [LambdaRuntime.PYTHON_3_8],
+      description: 'Layer for CRUD memes Lambda containing boto3'
+    })
+
     // create a Lambda function handling basic CRUD operations on the memes in the S3 folder
     const crudLambda = new LambdaFunction(this, Utilities.RESOURCE_NAMES.lambda.memesCrudLambda.lambdaName, {
-      runtime: Runtime.PYTHON_3_8,
+      runtime: LambdaRuntime.PYTHON_3_8,
       handler: Utilities.getLambdaHandlerByLambdaSourceCodeFilename(Utilities.RESOURCE_NAMES.lambda.memesCrudLambda.lambdaSourceCode),
-      code: LambdaSourceCode.fromAsset(Utilities.LAMBDA_SOURCE_CODE_FOLDER)
+      code: LambdaSourceCode.fromAsset(Utilities.LAMBDA_SOURCE_CODE_FOLDER),
+      layers: [crudLambdaLayer]
     })
 
     // create a DynamoDB table storing information regarding the uploaded memes in the S3 bucket
